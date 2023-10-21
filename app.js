912286d698710952d216d01c49b5a2eb0f54bbc8
Prince Darko <princedarko@Princes-MacBook-Pro.local>
2023-10-21 15:21:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+
+tl.to(".text", { y: "0%", duration: 1.5, stagger: 0.25 });
+
+tl.to(".slider", { y: "-100%", duration: 1.5, delay:0.2});
+
+tl.to(".intro", {y:"-100%", duration: 1}, "-=1.5");
+
+tl.fromTo(".content", {opacity:0}, {opacity:1, duration:1});
+
+tl.fromTo(".main-content", {opacity:0}, {opacity:1, duration:1}, "-=1");
+
+
+
