0e45f3544071bb664dca7d458d89c87795e64659
pdarko99 <padarko99@gmail.com>
2024-01-06 13:14:20 +0000

    worked on clients section, dispayed count of number of clients and all clients worked with

--- assets/animate_on_scroll.js
+++ assets/animate_on_scroll.js
@@ -12,6 +12,41 @@ const hiddenElements = document.querySelectorAll(".hidden");
 
 hiddenElements.forEach((element) => {
   observer.observe(element);
-})
+});
+
+const counterObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      counters.forEach((counter) => {
+        let initial_count = 0;
+        const final_count = counter.dataset.count;
+
+        const counting = setInterval(updateCounting, 10);
+        function updateCounting() {
+          initial_count = initial_count + 1;
+          counter.innerText = initial_count + "+";
+
+          if (initial_count >= final_count) {
+            clearInterval(counting);
+            // Stop observing once the counting is done
+            observer.unobserve(entry.target);
+          }
+        }
+      });
+    }
+  });
+});
+
+// Assume counters is an array of elements you want to observe
+// counters.forEach((counter) => {
+//   counterObserver.observe(counter);
+// });
+
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counterObserver.observe(counter);
+});
 
 
